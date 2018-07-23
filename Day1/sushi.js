00f375eb7b7305ab188d847bd45c17f77e776f7e
Atndesign <atnnetworkk@gmail.com>
2018-07-23 02:55:21 +0200

    da

--- Day1/sushi.js
+++
@@ -1,9 +0,0 @@
-var choices = ["bounce","shake","wobble","bounceInLeft","bounceOutDown","fadeIn","fadeInLeftBig","fadeInUpBig","flash","headShake"] 
-const sushi = document.querySelectorAll('img');
-
-sushi.forEach(img => {
-    img.addEventListener('click',function(){
-        var random =  choices[Math.floor(Math.random() * choices.length)];
-        img.className = "animated "+random
-    })
-});
